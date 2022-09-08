b90d62ce18bccb2ef4c72ac617d2a3196800566e
fazalrehan0 <110965274+fazalrehan0@users.noreply.github.com>
2022-09-08 09:31:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+df=pd.read_csv('/content/Sentiment Data - audit_data.csv')
+header=st.container()
+input=st.container()
+model=st.container()
+with header:
+ st.title('MY FIRST MODEL')
+ st.text('hello in the model')
+ st.write(df.head())
+ disc=pd.DataFrame(df.sentiment.value_counts())
+ st.bar_chart(disc)
+with input:
+ st.header('Enter the input')
+ fcol,scol=st.columns(2)
+ max_depth=fcol.slider('what should be the max?',min_value=10,max_value=100,value=30,step=10)
+ estimate=scol.selectbox('what should be the estimate?',options=[100,200,300,'None'],index=0)
+ ip=scol.text_input("Enter the text","Default")
+ 
+with model:
+ st.header('PREDICTIONS')
