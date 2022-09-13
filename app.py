f8c9dfd6a72ec714e5a6f754adce8fca5995c0af
fazalrehan0 <110965274+fazalrehan0@users.noreply.github.com>
2022-09-13 10:12:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pandas as pd
 df=pd.read_csv('Sentiment Data - audit_data.csv')
 header=st.container()
 input=st.container()
-model=st.container()
+output=st.container()
+
 with header:
  st.title('MY FIRST MODEL')
  st.text('hello in the model')
@@ -11,12 +12,17 @@ with header:
  disc=pd.DataFrame(df.sentiment.value_counts())
  st.bar_chart(disc)
 with input:
- st.header('Enter the input')
- fcol,scol=st.columns(2)
- max_depth=fcol.slider('what should be the max?',min_value=10,max_value=100,value=30,step=10)
- estimate=scol.selectbox('what should be the estimate?',options=[100,200,300,'None'],index=0)
- ip=scol.text_input("Enter the text","Default")
+ st.header('Enter the Dialog conversation')
+ ip=scol.text_input("Enter the text seperated by '.'","Default")
+ inp=ip.split('.')
+ with open('readme.txt', 'w') as f:
+  for i in inp:f.write(i+'\n')
+ 
+#  fcol,scol=st.columns(2)
+#  max_depth=fcol.slider('what should be the max?',min_value=10,max_value=100,value=30,step=10)
+#  estimate=scol.selectbox('what should be the estimate?',options=[100,200,300,'None'],index=0)
+#  ip=scol.text_input("Enter the text","Default")
  
 with model:
  st.header('PREDICTIONS')
- st.subheader('the predictions are')
+ #st.subheader('the predictions are')
